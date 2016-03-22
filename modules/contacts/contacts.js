726756d749e78029e4c0443326cb8456efb081b0
may <may@470b2c59-2f82-4072-9d40-d544e64e7199>
2016-03-22 13:29:21 +0000

    Prototyping some of the common functions

--- modules/contacts/contacts.js
+++ modules/contacts/contacts.js
@@ -39,65 +39,14 @@ window.DEF.modules.contacts.cmds = {
 	/**
 	 * Edit a contact
 	 */
-	edit: Backbone.Marionette.ItemView.extend({
-		tagName: "table",
-		className: "table table-full table-left",
+	edit: Roadtrip.Edit.extend({
 		template: require("./templates/edit.html"),
-		ui: {
-			"field": ".field",
-			"save": "#save",
-			"cancel": "#cancel",
-			"delete": "#delete"
-		},
-		events: {
-			"change @ui.field": "MakeDirty",
-			"click @ui.save": "Save",
-			"click @ui.cancel": "Cancel",
-			"click @ui.delete": "Delete"
-		},
-		onBeforeRender: function () {
-			if (!this.model) {
-				this.model = new DEF.modules.contacts.Model({})
-			}
-		},
-		MakeDirty: function (e) {
-			console.log(e);
-			if (e.currentTarget.value == this.model.get(e.currentTarget.id))
-				$(e.currentTarget).removeClass("dirty");
-			else
-				$(e.currentTarget).addClass("dirty");
-		},
-		Save: function (e) {
-			var model = this.model;
-			$(".field.dirty").each(function (i, $el) {
-				console.log($el.id, $el.value)
-				model.set($el.id, $el.value);
-			})
-			if (!this.model.id)
-				APP.models.contacts.create(model);
-			else
-				this.model.set('edits', this.model.get('edits') + 1);
-			this.triggerMethod('main:list');
-		},
-		Cancel: function (e) {
-			this.triggerMethod('main:list');
-		},
-		Delete: function (e) {
-			APP.models.contacts.remove(this.model);
-			this.triggerMethod('main:list');
-		}
 	}),
 	/**
 	 * View a plain, read-only contact
 	 */
-	view: Backbone.Marionette.ItemView.extend({
-		tagName: "table",
-		className: "table table-full table-left",
+	view: Roadtrip.View.extend({
 		template: require("./templates/view.html"),
-		onShow: function () {
-			console.log("x");
-			this.model.set('views', this.model.get('views') + 1);
-		}
 	})
 }
 
