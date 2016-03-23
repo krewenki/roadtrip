fc4f3e978370aa5d163e075dc7d22c4b77b14ba1
may <may@470b2c59-2f82-4072-9d40-d544e64e7199>
2016-03-23 20:23:37 +0000

    forgot to add files

---
+++ modules/orders/lineitem.js
@@ -0,0 +1,10 @@
+//DEF.modules.orders.Order_LineItems = Roadtrip.Collection.extend({
+//	//	model: DEF.modules.orders.Model,
+//	//	url: 'dev.telegauge.com:3000/roadtrip/orders_lineitems',
+//	//	comparator: "SOLI"
+//});
+//
+//
+DEF.modules.orders.LineItems = Backbone.Marionette.CollectionView.extend({
+
+})
