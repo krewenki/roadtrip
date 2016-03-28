e9dcb3797a40c1d3f6cb681bbb3a1df2c2e1325e
may <may@470b2c59-2f82-4072-9d40-d544e64e7199>
2016-03-28 12:05:01 +0000

    Projects

---
+++ roadtrip/modules/projects/projects.js
@@ -0,0 +1,116 @@
+DEF.modules.projects = {}
+
+/**
+ * The main model.  SHould be called "Model"
+ */
+DEF.modules.projects.Model = Roadtrip.Model.extend({
+	nameAttribute: 'name', // the human-readable field in the record
+	defaults: {
+		project: "Project 1",
+		icon: "employee",
+
+		edits: 0,
+		views: 0
+	},
+});
+
+/**
+ * The main collection.  MUST be called "Collection"
+ */
+DEF.modules.projects.Collection = Roadtrip.Collection.extend({
+	model: DEF.modules.projects.Model,
+	url: 'dev.telegauge.com:3000/roadtrip/projects',
+});
+APP.models.projects = new DEF.modules.projects.Collection()
+
+/**
+ * A list of commands, automatically tied to the $cmd in  #module/$cmd/$id.  See DoView
+ */
+DEF.modules.projects.views = {
+	/**
+	 * Edit a project
+	 */
+	edit: Roadtrip.Edit.extend({
+		module: "projects",
+		template: require("./templates/edit.html"),
+	}),
+	/**
+	 * View a plain, read-only single record
+	 */
+	view: Roadtrip.View.extend({
+		module: "projects",
+		template: require("./templates/view.html"),
+		ui: {
+			edit: "#edit",
+			delete: "#delete"
+		},
+		events: {
+			"click @ui.edit": "Edit",
+			"click @ui.delete": "Delete"
+		},
+		Edit: function () {
+			APP.Route("#projects/" + "edit" + "/" + this.model.id);
+		},
+		Delete: function () {
+			if (confirm("Are you sure you want to delete " + this.model.get(this.model.nameAttribute))) {
+				console.log("kill it");
+				APP.models.projects.remove(this.model);
+				APP.Route("#projects", "projects");
+			}
+		}
+	})
+}
+
+/**
+ * A single line of projects on the main project view
+ */
+DEF.modules.projects.RecordLine = Roadtrip.RecordLine.extend({
+	module: "projects",
+	template: require("./templates/project_box.html"),
+});
+
+/**
+ * The MainView.  HAS to be called MainView.  This is where this module begins
+ */
+DEF.modules.projects.MainView = Roadtrip.RecordList.extend({
+	id: 'PROJECTS',
+	template: require("./templates/projects.html"),
+	templateHelpers: function (x, y, z) {
+		return {
+			search: this.search,
+		}
+	},
+	childView: DEF.modules.projects.RecordLine,
+	childViewContainer: "#record_list",
+	ui: {
+		search: "#search",
+		add: "#add"
+	},
+	events: {
+		"keyup @ui.search": "Search",
+		"click @ui.add": "Add"
+	},
+	filter: function (model, index, collection) {
+		var string = model.search_string();
+		if (string.indexOf(this.ui.search.val().toUpperCase()) == -1)
+			return false;
+		return true;
+	},
+	initialize: function () {
+		APP.Icon_Lookup["Vendor"] = "building";
+	},
+	onRender: function () {
+		this.ui.search.focus().val(this.search); // this search is disgusting
+	},
+	Search: function (e) {
+		console.log(this.ui.search.val(), this.templateHelpers());
+		this.search = this.ui.search.val();
+		this.render();
+	},
+	Add: function () {
+		var page = new DEF.modules.projects.views.edit({
+			model: false,
+		});
+		APP.root.showChildView('main', page);
+	}
+});
