0d40a8314527bb9539e715e55eb7fe582d4d088f
Warren Krewenki <krewenki@gmail.com>
2016-03-28 17:52:50 +0000

    An ugly but semi functional calendar

--- modules/calendar/calendar.js
+++ modules/calendar/calendar.js
@@ -10,7 +10,10 @@ DEF.modules.calendar.Model = Roadtrip.Model.extend({
 	idAttribute: '_id',
 	nameAttribute: 'date', // the human-readable field in the record
 	defaults: {
-		order: "2016-01-01",
+		date: new Date().toISOString().slice(0,10),
+		title: 'New Event',
+		timestamp: new Date().getTime(),
+		notes: '',
 		views: 0,
 		edits: 0
 	},
@@ -28,25 +31,27 @@ DEF.modules.calendar.Collection = Roadtrip.Collection.extend({
 	url: 'dev.telegauge.com:3000/roadtrip/calendar',
 });
 APP.models.calendar = new DEF.modules.calendar.Collection()
-/**
- * A list of commands, automatically tied to the $cmd in  #module/$cmd/$id.  See DoView
- */
-DEF.modules.calendar.cmds = {
+
+
+DEF.modules.calendar.views = {
 	/**
-	 * Edit a contact
+	 * Edit a calendar
 	 */
 	edit: Roadtrip.Edit.extend({
 		module: "calendar",
 		template: require("./templates/edit.html"),
 	}),
+	inlineEvent: Roadtrip.View.extend({
+		module: "calendar",
+		template: require("./templates/inlineevent.html")
+	}),	
+
 	/**
-	 * View a plain, read-only contact
+	 * View a plain, read-only single record
 	 */
-	view: Backbone.Marionette.LayoutView.extend({
+	view: Roadtrip.View.extend({
+		module: "calendar",
 		template: require("./templates/view.html"),
-		regions: {
-			order: "#calendar"
-		},
 		ui: {
 			edit: "#edit",
 			delete: "#delete"
@@ -55,32 +60,41 @@ DEF.modules.calendar.cmds = {
 			"click @ui.edit": "Edit",
 			"click @ui.delete": "Delete"
 		},
-		onShow: function () {
-			this.model.set('views', this.model.get('views') + 1);
-
-			/*this.showChildView('order', new DEF.modules.orders.OrderView({
-				model: this.model,
-			}))
-			this.showChildView('lineitems', new DEF.modules.orders.LineItemView({
-				collection: new Backbone.Collection(this.model.get('lineitems'))
-			}))*/
-		},
 		Edit: function () {
 			APP.Route("#calendar/" + "edit" + "/" + this.model.id);
 		},
 		Delete: function () {
 			if (confirm("Are you sure you want to delete " + this.model.get(this.model.nameAttribute))) {
 				console.log("kill it");
-				APP.models.calendar.remove(this.model);
-				APP.Route("#orders", "orders");
+				APP.models.contacts.remove(this.model);
+				APP.Route("#contacts", "contacts");
 			}
 		}
 	})
 }
 
-DEF.modules.calendar.CalendarView = Backbone.Marionette.ItemView.extend({
-	template: require("./templates/calendar.html"),
-})
+DEF.modules.calendar.views.inlineDay = 	Backbone.Marionette.CompositeView.extend({
+		module: "calendar",
+		template: require("./templates/inlineday.html"),
+		childView: DEF.modules.calendar.views.inlineEvent,
+		initialize: function(options){
+			this.options = options;
+			//this.render()
+		},
+		ui: {
+			view: ".event"
+		},
+		events: {
+			"dblclick @ui.view": "View"
+		},
+		View: function(){
+			APP.Route("#calendar/" + "edit" + "/" + this.model.id);
+		},
+		SetDate: function(date){
+			this.date = date;
+		}
+});
+
 
 /**
  * The MainView.  HAS to be called MainView.  This is where this module begins
@@ -92,52 +106,28 @@ DEF.modules.calendar.MainView = Roadtrip.MainView.extend({
 	icons: {
 
 	},
-	Command: function (cmd, id) {
-		console.log(cmd, id);
-		var mode = this.id.toLocaleLowerCase();
-		switch (cmd) {
-		case 'view':
-			this.view = new DEF.modules[mode].cmds[cmd]({
-				model: APP.models[mode].get(id),
-			});
-			this.showChildView('list', this.view);
-			break;
-		default:
-			Roadtrip.MainView.prototype.Command.apply(this, arguments);
-		}
+	ui: {
+		search: "#search",
+		add: "#add"
 	},
-});
-
-/**
- * A single line of orders on the main order view
- */
-DEF.modules.calendar.RecordLine = Roadtrip.RecordLine.extend({
-	module: "calendar",
-	template: require("./templates/day.html"),
-	templateHelpers: function () {
-		var rs = {
-			total: ' ',
-			lineitems: 0
-		}
-		var lineitems = this.model.get('lineitems');
-		var sum = 0;
-		for (var l = 0; l < lineitems.length; l++)
-			sum += lineitems[l].price;
-		if (sum)
-			rs.total = APP.Format.money(sum);
-
-		rs.lineitems = lineitems.length;
-
-		return rs;
+	events: {
+		"keyup @ui.search": "Search",
+		"click @ui.add": "Add"
+	},
+	Add: function () {
+		var page = new DEF.modules.calendar.views.edit({
+			model: false,
+		});
+		APP.root.showChildView('main', page);
+	},
+	onShow: function(){
+		$('.calendar-day').each(function(){
+			var v = new DEF.modules.calendar.views.inlineDay({
+				el: $(this).find('.calendar_events'),
+				collection: new Backbone.Collection(APP.models.calendar.where({ date: '2016-03-'+$(this).attr('data-date') })),
+			})
+			v.render();
+		})
 	}
 });
 
-/**
- * This is a list of orders
- */
-DEF.modules.calendar.RecordList = Roadtrip.RecordList.extend({
-	module: "calendar",
-	template: require("./templates/day.html"),
-	childView: DEF.modules.calendar.RecordLine,
-
-})
