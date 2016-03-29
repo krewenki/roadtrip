40657edc818787946a9b97a2e36cd558b11433a8
may <may@470b2c59-2f82-4072-9d40-d544e64e7199>
2016-03-29 19:07:14 +0000

    Some CSS work and beautify work

--- roadtrip/modules/tasks/tasks.js
+++ roadtrip/modules/tasks/tasks.js
@@ -1,161 +1,162 @@
 DEF.modules.tasks = {};
 DEF.modules.tasks.Router = Roadtrip.Router.extend({
-  initialize: function() {
-    APP.models.tasks = new DEF.modules.tasks.Collection();
-  },
-  module: "tasks",
-  routes: {
-    "tasks/:cmd/:arg": "LoadModule",
-  },
+	initialize: function() {
+		APP.models.tasks = new DEF.modules.tasks.Collection();
+	},
+	module: "tasks",
+	routes: {
+		"tasks/:cmd/:arg": "LoadModule",
+	},
 });
 
 DEF.modules.tasks.Model = Roadtrip.Model.extend({
-  idAttribute: '_id',
-  nameAttribute: 'task', // the human-readable field in the record
-  defaults: {
-    parent_id: false,
-    parent_module: false,
+	idAttribute: '_id',
+	nameAttribute: 'task', // the human-readable field in the record
+	defaults: {
+		parent_id: false,
+		parent_module: false,
 
-    kind: "todo",
-    task: "Do a thing",
-    description: "",
-    task_id: "0.0.0",
+		kind: "todo",
+		task: "Do a thing",
+		description: "",
+		task_id: "0.0.0",
 
-    start_date: false,
-    due_date: false,
+		start_date: false,
+		due_date: false,
 
-    progress: 0, // scale 0..100
-    priority: 0, // scale 0..100
+		progress: 0, // scale 0..100
+		priority: 0, // scale 0..100
 
-    views: 0,
-    edits: 0
-  },
-  search_string: function() {
-    var string = this.get('task') + "";
-    return string.toUpperCase();
-  }
+		views: 0,
+		edits: 0
+	},
+	search_string: function() {
+		var string = this.get('task') + "";
+		return string.toUpperCase();
+	}
 });
 
 DEF.modules.tasks.Collection = Roadtrip.Collection.extend({
-  model: DEF.modules.tasks.Model,
-  url: 'dev.telegauge.com:3000/roadtrip/tasks',
-  comparator: function(m) {
-    return m.get('progress') - m.get('priority')
-  }
+	model: DEF.modules.tasks.Model,
+	url: 'dev.telegauge.com:3000/roadtrip/tasks',
+	comparator: function(m) {
+		return m.get('progress') - m.get('priority')
+	}
 });
 
 /**
  * A single line, showing a task on the Project page
  */
 DEF.modules.tasks.TaskView = Backbone.Marionette.ItemView.extend({
-  template: require("./templates/taskline.html"),
-  className: "click hover",
-  tagName: "tr",
-  events: {
-    "click": "ViewTask"
-  },
-  ViewTask: function() {
-    APP.Route("#tasks/view/" + this.model.get('_id'));
-  }
+	template: require("./templates/taskline.html"),
+	className: "click hover",
+	tagName: "tr",
+	events: {
+		"click": "ViewTask"
+	},
+	ViewTask: function() {
+		APP.Route("#tasks/view/" + this.model.get('_id'));
+	}
 });
 
 DEF.modules.tasks.views = {
-  /**
-   * Edit a contact
-   */
-  edit: Roadtrip.Edit.extend({
-    module: "tasks",
-    template: require("./templates/task_edit.html"),
-    Return: function() {
-      if (this.model.get('parent_id'))
-        APP.Route("#" + this.model.get('parent_module') + "/view/" + this.model.get('parent_id'))
-      else
-        APP.Route("#");
-    },
-    templateHelpers: function() {
-      var rs = {
-        task_id: this.GenerateTaskID()
-      };
-      if (this.options.parent) {
-        rs.parent_id = this.options.parent.id;
-        rs.parent_module = this.options.parent.module;
-      }
-      console.log(rs, this.options);
-      return rs;
-    },
-    GenerateTaskID: function() {
-      if (this.model.id)
-        return this.model.get('task_id');
+	/**
+	 * Edit a contact
+	 */
+	edit: Roadtrip.Edit.extend({
+		module: "tasks",
+		template: require("./templates/task_edit.html"),
+		Return: function() {
+			if (this.model.get('parent_id'))
+				APP.Route("#" + this.model.get('parent_module') + "/view/" + this.model.get('parent_id'))
+			else
+				APP.Route("#");
+		},
+		templateHelpers: function() {
+			var rs = {
+				task_id: this.GenerateTaskID()
+			};
+			if (this.options.parent) {
+				rs.parent_id = this.options.parent.id;
+				rs.parent_module = this.options.parent.module;
+			}
+			console.log(rs, this.options);
+			return rs;
+		},
+		GenerateTaskID: function() {
+			if (this.model.id) // this model has been saved
+				return this.model.get('task_id'); // so do not generate a task_id
 
-      prefix = false;
-      var parent = APP.models[this.options.parent.module].get(this.options.parent.id);
-      if (parent)
-        prefix = parent.get('task_id');
-      var instance = APP.models.tasks.where({
-        parent_module: this.options.parent.module,
-        parent_id: this.options.parent.id
-      }).length
-      if (prefix)
-        return prefix + "." + (instance + 1);
-      else
-        return instance
-    }
-  }),
+			prefix = false;
+			var parent = APP.models[this.options.parent.module].get(this.options.parent.id);
+			if (parent)
+				prefix = parent.get('task_id');
+			var instance = APP.models.tasks.where({
+				parent_module: this.options.parent.module,
+				parent_id: this.options.parent.id
+			}).length + 1
+			if (prefix)
+				return prefix + "." + instance;
+			else
+				return instance
+		}
+	}),
 
-  /**
-   * View a plain, read-only single record
-   */
-  view: Backbone.Marionette.CompositeView.extend({
-    id: "TASKS",
-    module: "tasks",
-    template: require("./templates/task_view.html"),
-    childView: DEF.modules.tasks.TaskView,
-    childViewContainer: "#task_list",
-    ui: {},
-    collection: APP.models.tasks, // why cant we do this?
-    onBeforeRender: function() {
-      this.collection = APP.models.tasks; // whatever, man..
-      this.filter = function(m) {
-        return m.get('parent_id') == this.model.id
-      }
-    },
-    onShow: function() {
-      if (this.children.length == 0) {
-        this.ui.subtasks.hide();
-      }
-    },
-    ui: {
-      edit: "#edit",
-      subtask: "#subtask",
-      subtasks: "#subtasks",
-      progress: "#progress"
-    },
-    initialize: function() {
-      APP.Icon_Lookup["todo"] = "list-ul";
-      APP.Icon_Lookup["bug"] = "bug";
-      APP.Icon_Lookup["feature"] = "lightbulb-o";
-    },
-    events: {
-      "click @ui.edit": "Edit",
-      "click @ui.subtask": "AddSubtask",
-      "click @ui.progress": "UpdateProgress"
-    },
-    Edit: function() {
-      APP.Route("#tasks/edit/" + this.model.id);
-    },
-    AddSubtask: function() {
-      var page = new DEF.modules.tasks.views.edit({
-        model: false,
-        parent: {
-          module: "tasks",
-          id: this.model.id
-        }
-      });
-      APP.root.showChildView('main', page);
-    },
-    UpdateProgress: function(e) {
-      console.log("progress", this.ui.progress.val());
-      this.model.set('progress', this.ui.progress.val());
-    }
-  })
+	/**
+	 * View a plain, read-only single record
+	 */
+	view: Backbone.Marionette.CompositeView.extend({
+		id: "TASKS",
+		module: "tasks",
+		template: require("./templates/task_view.html"),
+		childView: DEF.modules.tasks.TaskView,
+		childViewContainer: "#task_list",
+		ui: {},
+		collection: APP.models.tasks, // why cant we do this?
+		onBeforeRender: function() {
+			this.collection = APP.models.tasks; // whatever, man..
+			this.filter = function(m) {
+				return m.get('parent_id') == this.model.id
+			}
+		},
+		onShow: function() {
+			if (this.children.length == 0) {
+				this.ui.subtasks.hide();
+			}
+			APP.SetTitle(this.model.get(this.model.nameAttribute));
+		},
+		ui: {
+			edit: "#edit",
+			subtask: "#subtask",
+			subtasks: "#subtasks",
+			progress: "#progress"
+		},
+		initialize: function() {
+			APP.Icon_Lookup["todo"] = "list-ul";
+			APP.Icon_Lookup["bug"] = "bug";
+			APP.Icon_Lookup["feature"] = "lightbulb-o";
+		},
+		events: {
+			"click @ui.edit": "Edit",
+			"click @ui.subtask": "AddSubtask",
+			"click @ui.progress": "UpdateProgress"
+		},
+		Edit: function() {
+			APP.Route("#tasks/edit/" + this.model.id);
+		},
+		AddSubtask: function() {
+			var page = new DEF.modules.tasks.views.edit({
+				model: false,
+				parent: {
+					module: "tasks",
+					id: this.model.id
+				}
+			});
+			APP.root.showChildView('main', page);
+		},
+		UpdateProgress: function(e) {
+			console.log("progress", this.ui.progress.val());
+			this.model.set('progress', this.ui.progress.val());
+		}
+	})
 }
