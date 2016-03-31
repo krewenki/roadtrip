09ad14ac182e76a9ce662a913cba5ac4d70ae935
Warren Krewenki <krewenki@gmail.com>
2016-03-31 23:47:46 +0000

    Fix broken auth

--- app/auth.js
+++ app/auth.js
@@ -4,10 +4,11 @@ if(document.cookie == ''){
 } else {
 	var c = document.cookie.split(';');
 	var co = c.map(function(s){ return s.split('=')});
-	var cookie = {};
+	var cookie = {
+		user: false
+	};
 	for(var i in co){
 		cookie[co[i][0].trim()] = decodeURIComponent(co[i][1]);
 	}
 	window.U = JSON.parse(cookie.user);
 }
-
