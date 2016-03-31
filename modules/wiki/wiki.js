a7da47abf929dced899b96beffd84389b9ba80a3
may <may@470b2c59-2f82-4072-9d40-d544e64e7199>
2016-03-31 00:03:17 +0000

    wiki

---
+++ modules/wiki/wiki.js
@@ -0,0 +1,27 @@
+DEF.modules.wiki = {};
+DEF.modules.wiki.Router = Roadtrip.Router.extend({
+	initialize: function() {},
+	module: "wiki",
+	routes: {
+		"wiki/:path": "LoadWiki",
+	},
+	LoadWiki: function(path) {
+		console.log("Got path", path)
+	}
+});
+
+DEF.modules.wiki.Model = Roadtrip.Model.extend({
+	nameAttribute: 'title',
+	defaults: {
+		title: "Wiki 1",
+		content: ""
+	}
+})
+
+DEF.modules.wiki.Collection = Backbone.Collection.extend({
+	model: DEF.modules.wiki.Model
+})
+
+DEF.modules.wiki.Article = Backbone.Marionette.LayoutView.extend({
+	template: require("./templates/article.html")
+})
