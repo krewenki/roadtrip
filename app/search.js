2fe7047baa5a099f75e86b36fa1f2ddd26fdd25e
may <may@470b2c59-2f82-4072-9d40-d544e64e7199>
2016-03-31 13:51:36 +0000

    Search templates

---
+++ app/search.js
@@ -0,0 +1,37 @@
+DEF.Search_Result = Backbone.Marionette.ItemView.extend({
+	className: "search_result click",
+	template: require("../templates/search_result.html"),
+	templateHelpers: function() {
+		return {
+			term: this.model.search_string(),
+			icon: this.model.icon()
+		}
+	},
+	events: {
+		"click": "Go"
+	},
+	Go: function() {
+		$("#search").val("")
+		$("#SEARCH").hide();
+		APP.Route(this.model.GetLink("view"))
+	}
+})
+
+DEF.Search = Backbone.Marionette.CompositeView.extend({
+	template: require("../templates/search_results.html"),
+	childView: DEF.Search_Result,
+	childViewContainer: "#results",
+	onBeforeRender: function() {
+		var search = this.options.search.toUpperCase();
+		var matches = [];
+		var modules = Object.keys(APP.models);
+		for (var m = 0; m < modules.length; m++) {
+			var module = modules[m];
+			APP.models[module].each(function(model, index) {
+				if (index < 15 && model.search_string().toUpperCase().indexOf(search) != -1)
+					matches.push(model);
+			})
+		}
+		this.collection = new Backbone.Collection(matches);
+	},
+})
