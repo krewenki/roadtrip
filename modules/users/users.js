822f2e76542e3543010177dbf05a7c5702e367db
may <may@470b2c59-2f82-4072-9d40-d544e64e7199>
2016-04-01 18:01:50 +0000

    Refs 19.1 - perm setting on Users sheet

--- modules/users/users.js
+++ modules/users/users.js
@@ -14,7 +14,46 @@ DEF.modules.users.Model = Roadtrip.Model.extend({
 	nameAttribute: 'name', // the human-readable field in the record
 	module: "users",
 	defaults: {
-		name: "Joe"
+		name: "Joe",
+		email: "crap@fart.com",
+		perms: {
+			tasks: {
+				create: false,
+				read: true,
+				update: false,
+				delete: false
+			},
+			orders: {
+				create: false,
+				read: true,
+				update: false,
+				delete: false
+			},
+			contacts: {
+				create: false,
+				read: true,
+				update: false,
+				delete: false
+			},
+			projects: {
+				create: false,
+				read: true,
+				update: false,
+				delete: false
+			},
+			calendar: {
+				create: false,
+				read: true,
+				update: false,
+				delete: false
+			},
+			users: {
+				create: false,
+				read: true,
+				update: false,
+				delete: false
+			},
+		}
 	}
 });
 
@@ -30,6 +69,13 @@ DEF.modules.users.views = {
 	edit: Roadtrip.Edit.extend({
 		module: "users",
 		template: require("./templates/edit.html"),
+		// Save: function() {
+		// 	return Roadtrip.Edit.prototype.Save.call(this)
+		// 	debugger
+		// 	$(".field.dirty").each(function(i, $el) {
+		//
+		// 	})
+		// }
 	}),
 	/**
 	 * View a plain, read-only single record
@@ -44,10 +90,41 @@ DEF.modules.users.RecordLine = Roadtrip.RecordLine.extend({
 	tagName: "tr",
 	module: "users",
 	template: require("./templates/user_line.html"),
+	ui: {
+		perm: ".perms"
+	},
+	events: {
+		"click @ui.perm": "SetPerm"
+	},
+	SetPerm: function(e) {
+		$el = e.currentTarget;
+		console.log(e.currentTarget);
+		var parts = $el.id.split('.');
+		var perms = this.model.get('perms');
+		perms[parts[0]][parts[1]] = $el.checked;
+		this.model.set({
+			perms: perms
+		});
+		this.model.trigger('change', this.model);
+		console.log(perms);
+	}
 });
 
 DEF.modules.users.MainView = Roadtrip.RecordList.extend({
 	template: require("./templates/main.html"),
 	childView: DEF.modules.users.RecordLine,
 	childViewContainer: "#user_list",
+	ui: {
+		add: "#add"
+	},
+	events: {
+		"click @ui.add": "Add"
+	},
+	Add: function() {
+		var page = new DEF.modules.users.views.edit({
+			model: false,
+		});
+		APP.root.showChildView('main', page);
+	}
+
 })
