2817884eca054dbe5377d858dc0b15355a6920a1
may <may@470b2c59-2f82-4072-9d40-d544e64e7199>
2016-04-05 18:19:31 +0000

    Some footer works

--- roadtrip/modules/contacts/contacts.js
+++ roadtrip/modules/contacts/contacts.js
@@ -1,15 +1,15 @@
 DEF.modules.contacts = {}
-DEF.modules.contacts.Router = Roadtrip.Router.extend( {
+DEF.modules.contacts.Router = Roadtrip.Router.extend({
 	module: "contacts",
 	initialize: function() {
 		APP.models.contacts = new DEF.modules.contacts.Collection();
-		APP.Icon_Lookup[ "Vendor" ] = "building";
-		APP.Icon_Lookup[ "Customer" ] = "money";
-		APP.Icon_Lookup[ "Miscellaneous" ] = "question";
-		APP.Icon_Lookup[ "Customer,Marine" ] = "ship";
-		APP.Icon_Lookup[ "Insurance" ] = "wheelchair";
-		APP.Icon_Lookup[ "Freight Transportation" ] = "truck";
-		APP.Icon_Lookup[ "Services" ] = "thumbs-up";
+		APP.Icon_Lookup["Vendor"] = "building";
+		APP.Icon_Lookup["Customer"] = "money";
+		APP.Icon_Lookup["Miscellaneous"] = "question";
+		APP.Icon_Lookup["Customer,Marine"] = "ship";
+		APP.Icon_Lookup["Insurance"] = "wheelchair";
+		APP.Icon_Lookup["Freight Transportation"] = "truck";
+		APP.Icon_Lookup["Services"] = "thumbs-up";
 
 	},
 	routes: {
@@ -23,12 +23,12 @@ DEF.modules.contacts.Router = Roadtrip.Router.extend( {
 	//	Home: function () {
 	//		this.ShowRoot("contacts");
 	//	}
-} )
+})
 
 /**
  * The main model.  SHould be called "Model"
  */
-DEF.modules.contacts.Model = Roadtrip.Model.extend( {
+DEF.modules.contacts.Model = Roadtrip.Model.extend({
 	nameAttribute: 'name', // the human-readable field in the record
 	module: "contacts",
 	defaults: {
@@ -43,21 +43,21 @@ DEF.modules.contacts.Model = Roadtrip.Model.extend( {
 		zip: "",
 
 	},
-	GetLink: function( cmd ) {
-		return "#contacts/" + cmd + "/" + this.get( '_id' );
+	GetLink: function(cmd) {
+		return "#contacts/" + cmd + "/" + this.get('_id');
 	},
 	search_string: function() {
-		return this.get( 'name' );
+		return this.get('name');
 	}
-} )
+})
 
 /**
  * The main collection.  MUST be called "Collection"
  */
-DEF.modules.contacts.Collection = Roadtrip.Collection.extend( {
+DEF.modules.contacts.Collection = Roadtrip.Collection.extend({
 	model: DEF.modules.contacts.Model,
 	url: 'dev.telegauge.com:3000/roadtrip/contacts',
-} );
+});
 
 /**
  * A list of commands, automatically tied to the $cmd in  #module/$cmd/$id.  See DoView
@@ -66,34 +66,34 @@ DEF.modules.contacts.views = {
 	/**
 	 * Edit a contact
 	 */
-	edit: Roadtrip.Edit.extend( {
+	edit: Roadtrip.Edit.extend({
 		module: "contacts",
-		template: require( "./templates/edit.html" ),
-	} ),
+		template: require("./templates/edit.html"),
+	}),
 	/**
 	 * View a plain, read-only single record
 	 */
-	view: Roadtrip.View.extend( {
+	view: Roadtrip.View.extend({
 		module: "contacts",
-		template: require( "./templates/view.html" ),
-	} )
+		template: require("./templates/view.html"),
+	})
 }
 
 /**
  * A single line of contacts on the main contact view
  */
-DEF.modules.contacts.RecordLine = Roadtrip.RecordLine.extend( {
+DEF.modules.contacts.RecordLine = Roadtrip.RecordLine.extend({
 	module: "contacts",
-	template: require( "./templates/contact_line.html" ),
-} );
+	template: require("./templates/contact_line.html"),
+});
 
 /**
  * The MainView.  HAS to be called MainView.  This is where this module begins
  */
-DEF.modules.contacts.MainView = Roadtrip.RecordList.extend( {
+DEF.modules.contacts.MainView = Roadtrip.RecordList.extend({
 	id: 'CONTACTS',
-	template: require( "./templates/contacts.html" ),
-	templateHelpers: function( x, y, z ) {
+	template: require("./templates/contacts.html"),
+	templateHelpers: function(x, y, z) {
 		return {
 			search: this.search,
 		}
@@ -108,27 +108,27 @@ DEF.modules.contacts.MainView = Roadtrip.RecordList.extend( {
 		"keyup @ui.search": "Search",
 		"click @ui.add": "Add"
 	},
-	filter: function( model, index, collection ) {
+	filter: function(model, index, collection) {
 		var string = model.search_string();
-		if ( string.indexOf( this.ui.search.val().toUpperCase() ) == -1 )
+		if (string.indexOf(this.ui.search.val().toUpperCase()) == -1)
 			return false;
 		return true;
 	},
 	onShow: function() {
-		APP.SetTitle( "Contacts", "contacts" );
+		APP.SetTitle("Contacts", "contacts");
 	},
 	onRender: function() {
-		this.ui.search.focus().val( this.search ); // this search is disgusting
+		this.ui.search.focus().val(this.search); // this search is disgusting
 	},
-	Search: function( e ) {
-		console.log( this.ui.search.val(), this.templateHelpers() );
+	Search: function(e) {
+		console.log(this.ui.search.val(), this.templateHelpers());
 		this.search = this.ui.search.val();
 		this.render();
 	},
 	Add: function() {
-		var page = new DEF.modules.contacts.views.edit( {
+		var page = new DEF.modules.contacts.views.edit({
 			model: false,
-		} );
-		APP.root.showChildView( 'main', page );
+		});
+		APP.root.showChildView('main', page);
 	}
-} );
+});
