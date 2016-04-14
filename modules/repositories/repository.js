46092485e7e111b0aef8471152d069cb18818ab3
may <may@470b2c59-2f82-4072-9d40-d544e64e7199>
2016-04-14 20:13:04 +0000

    Trying to get real usernames to be in the database for revisions, but this may be impossible.  Refs #1.15.2"

--- modules/repositories/repository.js
+++ modules/repositories/repository.js
@@ -7,16 +7,16 @@ DEF.modules.repositories.Router = Roadtrip.Router.extend({
 DEF.modules.repositories.Model = Roadtrip.Model.extend({
 	defaults: {
 		name: "Main",
-    type: "svn",
-    url: "svn://repo.repo"
+		type: "svn",
+		url: "svn://repo.repo"
 	},
-	LookupUser: function(user){
-			var out = user;
-			var users = this.get('users');
-			if(users && users[user]){
-				out = APP.models.users.get(users[user]).get('name');
-			}
-			return out;
+	LookupUser: function(user) {
+		var out = user;
+		var users = this.get('users');
+		if (users && users[user]) {
+			out = users[user] //APP.models.users.get(users[user]).get('name');
+		}
+		return out;
 	}
 })
 DEF.modules.repositories.Collection = Backbone.Highway.Collection.extend({
