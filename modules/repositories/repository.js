dbaefab243f9f671b9d93be3cae281b98a844f80
Warren Krewenki <krewenki@gmail.com>
2016-04-14 14:23:18 +0000

    Repository module and showing real names in commit logs. refs #1.15.2

--- modules/repositories/repository.js
+++ modules/repositories/repository.js
@@ -9,6 +9,14 @@ DEF.modules.repositories.Model = Roadtrip.Model.extend({
 		name: "Main",
     type: "svn",
     url: "svn://repo.repo"
+	},
+	LookupUser: function(user){
+			var out = user;
+			var users = this.get('users');
+			if(users && users[user]){
+				out = APP.models.users.get(users[user]).get('name');
+			}
+			return out;
 	}
 })
 DEF.modules.repositories.Collection = Backbone.Highway.Collection.extend({
