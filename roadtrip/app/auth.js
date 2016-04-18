7defa3001bd228bf0bf01676e8204bb70470066b
wck <wck@470b2c59-2f82-4072-9d40-d544e64e7199>
2016-04-18 18:41:43 +0000

    auth.js modified so that it doesnt look at cookies.  refs #1.2.11

--- roadtrip/app/auth.js
+++ roadtrip/app/auth.js
@@ -1,27 +1,9 @@
-if (document.cookie == '') {
-	window.location.href = '/logout';
-} else {
-	var c = document.cookie.split(';');
-	var co = c.map(function(s) {
-		return s.split('=');
-	});
-	var cookie = {
-		user: APP.anon
-	};
-	for (var i in co) {
-		cookie[co[i][0].trim()] = co[i][1];
-	}
-
-	// We need to conveniently store the user.
 	DEF.modules.users.Initialize();
 
-	window.U = new DEF.modules.users.Model({
-		is_anonymous: true
-	});
+	window.U = new DEF.modules.users.Model(window.User);
 	APP.models.users.once('sync', function() {
-		var user = cookie.user;
 
-		window.U = APP.models.users.get(user); // assign 0 to the U.
+		window.U = APP.models.users.get(window.User._id); // assign 0 to the U.
 		APP.trigger("auth_user");
 		window.U.set({
 			last_login: Date.now()
@@ -29,4 +11,3 @@ if (document.cookie == '') {
 			trigger: false
 		});
 	}, this);
-}
