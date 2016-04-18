29420cbbc7f5af1fc3c8d96ce088b6b86980aa65
wck <wck@470b2c59-2f82-4072-9d40-d544e64e7199>
2016-04-18 13:39:35 +0000

    Remove index.html and serve a dynamic template in its place

--- roadtrip/server/config.js
+++ roadtrip/server/config.js
@@ -1,26 +1,34 @@
 var config = {
 	uri: 'dev.telegauge.com',
 	database: 'roadtrip',
-	auth: [
-		{
-			defaultUser: '56fea5cc54d49c036c802e53',
-			strategy: 'local',
-			sessionLength: 60*60*24*365,
-			forceRootAuth: false,
-			routes : {
-				login : '/#login',
-				passwordReset: '/password-reset'
+	auth: [ {
+		defaultUser: '56fea5cc54d49c036c802e53',
+		strategy: 'local',
+		sessionLength: 60 * 60 * 24 * 365,
+		forceRootAuth: false,
+		routes: {
+			login: '/#login',
+			passwordReset: '/password-reset'
+		}
+	} ],
+	email: {
+		transporter: 'smtps://krewenki%40gmail.com:rvbaveokrgpdfdda@smtp.gmail.com', // I guess we could use telegauge for now
+		messages: {
+			passwordReset: {
+				subject: 'Reset your roadtrip password',
+				template: __dirname + '/templates/email/password_reset.html', // pass a path, parse it when we need it
 			}
 		}
-	],
-	email: {
-			transporter: 'smtps://krewenki%40gmail.com:rvbaveokrgpdfdda@smtp.gmail.com', // I guess we could use telegauge for now
-			messages: {
-				passwordReset: {
-					subject: 'Reset your roadtrip password',
-					template: __dirname+'/templates/email/password_reset.html', // pass a path, parse it when we need it
-				}
-			}
+	},
+
+	onComplete: function () {
+		this.http.get( '/', function ( req, res ) {
+			var _ = require( 'underscore' );
+			var fs = require( 'fs' );
+			res.send( _.template( fs.readFileSync( '/Users/warren/Sites/roadtrip.svn/templates/application.html', {
+				encoding: 'utf8'
+			} ) )( req.session.passport ) )
+		} )
 	}
 };
 
