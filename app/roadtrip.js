abfbd7ebb19ce2195d6d183676c771e24fb59dde
may <may@470b2c59-2f82-4072-9d40-d544e64e7199>
2016-04-19 13:04:53 +0000

    Error checking on getUp.  #1.49

--- app/roadtrip.js
+++ app/roadtrip.js
@@ -193,12 +193,13 @@ window.Roadtrip = {
 		getUp: function(field) {
 			if (this.get(field))
 				return this.get(field);
-			var model = APP.models[this.get('parent_module')].get(this.get('parent_id'));
-			if (model) {
-				return model.getUp(field);
-			} else {
-				return false;
+			if (this.get('parent_module')) {
+				var model = APP.models[this.get('parent_module')].get(this.get('parent_id'));
+				if (model) {
+					return model.getUp(field);
+				}
 			}
+			return false;
 		},
 		/**
 		 * Use this to quickly set stats for the models
