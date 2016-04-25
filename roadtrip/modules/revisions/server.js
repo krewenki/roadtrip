1564e2d9ef257a042b7e8629c6c07ff49991d841
wck <wck@470b2c59-2f82-4072-9d40-d544e64e7199>
2016-04-25 13:52:45 +0000

    A fix to the shifting dates in calendar. refs #1.3

--- roadtrip/modules/revisions/server.js
+++ roadtrip/modules/revisions/server.js
@@ -1,10 +1,11 @@
 var hooks = {
-  beforeSave: function(data){
-    console.log('I am the before save hook')
-  },
-  afterSave: function(data){
-    console.log('I am the after save hook')
-  }
+	beforeSave: function ( data ) {
+		console.log( 'this is the before save hook' );
+		return data;
+	},
+	afterSave: function ( data ) {
+		console.log( 'I am the after save hook' )
+	}
 }
 
 module.exports = hooks;
