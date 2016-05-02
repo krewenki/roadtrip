83351531e33b496e60dc84e3903150d117828c17
wck <wck@470b2c59-2f82-4072-9d40-d544e64e7199>
2016-05-02 19:17:19 +0000

    exit handlers for server.js to help prevent excessive database connections

--- roadtrip/server.js
+++ roadtrip/server.js
@@ -62,3 +62,27 @@ config.http = app;
 config.io = io;
 
 var hw = new Highway(config)
+
+
+
+function exitHandler(options, err) {
+	//hw.CleanUp();
+	if (options.cleanup) console.log('clean');
+	if (err) console.log(err.stack);
+	if (options.exit) process.exit();
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null, {
+	cleanup: true
+}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {
+	exit: true
+}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {
+	exit: true
+}));
