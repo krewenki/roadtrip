31dc68e93a1e528707b3eede0ff9bb2a149b3bea
Warren Krewenki <krewenki@gmail.com>
2016-05-02 10:31:28 +0000

    SVN hook for repos that tranforms svn username into roadtrip user_id.  refs #1.15.2

--- modules/revisions/server.js
+++ modules/revisions/server.js
@@ -1,10 +1,21 @@
 var hooks = {
-	beforeSave: function ( data ) {
-		console.log( 'this is the before save hook' );
-		return data;
+	beforeSave: function (data) {
+		return new Promise(function (success, failure) {
+			// Transform author to user_id
+			var users = {
+				'wck': '56f3f7cb4b88de4618e306c0',
+				'may': '56fd29934b88de4618e306c5'
+			}
+
+			data.author = users[data.author];
+			success(data);
+
+
+		})
 	},
-	afterSave: function ( data ) {
-		console.log( 'I am the after save hook' )
+	afterSave: function (data) {
+		console.log('I am the after save hook')
+		return data;
 	}
 }
 
