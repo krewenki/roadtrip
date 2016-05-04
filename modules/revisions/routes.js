562af1b54f1818e14af36258bacd8eb496aa6c12
Warren Krewenki <krewenki@gmail.com>
2016-05-04 18:41:11 +0000

    Updates to github route

--- modules/revisions/routes.js
+++ modules/revisions/routes.js
@@ -12,16 +12,54 @@ var routes = [
 			var repo = repos[obj.repository.name];
 			// https://api.github.com/repos/krewenki/ reponame /commits/:sha
 			var commits = obj.commits,
-				commit, url;
+				commit, url, req;
 			for (var i in commits) {
 				commit = commits[i];
 				url = 'https://api.github.com/repos/krewenki/' + obj.repository.name + '/commits/' + commit.id;
-				request.get(url, function (d) {
-					console.log(d);
-				})
+				request({
+					url: url,
+					headers: {
+						'User-Agent': 'roadtrip'
+					}
+				}, function (error, response, body) {
+					req = JSON.parse(body);
+					// how do I access the database from here?
+					var files = [],
+						changed, removed, total_changed, total_removed, diff = '',
+						lines;
+					for (var i in req.files) {
+						changed = 0;
+						removed = 0;
+						diff += req.files[i].patch;
+						lines = req.files[i].patch.split("\n");
+						for (var n in lines) {
+							if (lines[n].charAt(0) == '+')
+								changed++;
+							if (lines[n].charAt(0) == '-')
+								removed--;
+						}
+						files.push({
+							changed: changed,
+							removed: removed,
+							file: req.files[i].filename
+						})
+						total_changed += changed;
+						total_removed += removed;
+					}
+					hw.db.createRecord('revisions', {
+						revision: commit.id,
+						repository: repo,
+						author: '56f3f7cb4b88de4618e306c0', // Not always warren, but hardcode for laziness now
+						datetime: new Date(req.commit.author.date).getTime(), // change this to the actual commit time?
+						log: req.commit.message,
+						changed: total_changed,
+						removed: total_removed,
+						diff_meta: files,
 
+
+					})
+				})
 			}
-			// obj.commits = array of commits.  fetch each SHA
 		}
 	}
 
