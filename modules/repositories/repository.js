9f0558b8501908230f3ad5197d4dc34a8534e1c8
Warren Krewenki <krewenki@gmail.com>
2016-05-05 09:40:28 +0000

    A worthless, but functional, repositories module that may get merged into projects anyway.

--- modules/repositories/repository.js
+++ modules/repositories/repository.js
@@ -1,27 +1,51 @@
 DEF.modules.repositories = {};
-DEF.modules.repositories.Initialize = function() {
+DEF.modules.repositories.Initialize = function () {
 	if (!APP.models.repositories)
 		APP.models.repositories = new DEF.modules.repositories.Collection();
 };
 DEF.modules.repositories.Router = Roadtrip.Router.extend({
-	initialize: function() {},
+	module: "repositories",
+	collections: [
+		"repositories", "revisions", "tasks"
+	],
+	routes: {
+		"repositories/:cmd/:arg": "LoadModule",
+		"repositories": "ShowRoot"
+	},
 });
 DEF.modules.repositories.Model = Roadtrip.Model.extend({
 	defaults: {
 		name: "Main",
 		type: "svn",
 		url: "svn://repo.repo"
-	},
-	LookupUser: function(user) {
-		var out = user;
-		var users = this.get('users');
-		if (users && users[user]) {
-			out = users[user]; //APP.models.users.get(users[user]).get('name');
-		}
-		return out;
 	}
 });
 DEF.modules.repositories.Collection = Backbone.Highway.Collection.extend({
 	model: DEF.modules.repositories.Model,
 	url: 'roadtrip.telegauge.com/roadtrip/repositories',
 });
+
+
+DEF.modules.repositories.views = {
+	view: Roadtrip.View.extend({
+		module: 'repositories',
+		template: require('./templates/view.html')
+	}),
+	RepositoryLine: Roadtrip.RecordLine.extend({
+		module: 'repositories',
+		template: require('./templates/line.html'),
+		tagName: 'tr'
+	})
+}
+
+
+DEF.modules.repositories.MainView = Roadtrip.RecordList.extend({
+	id: 'REPOSITORIES',
+	template: require("./templates/main.html"),
+	childView: DEF.modules.repositories.views.RepositoryLine,
+	childViewContainer: '#record_list',
+	onShow: function () {
+		if (!this.children.length)
+			this.$el.parent().hide();
+	}
+})
