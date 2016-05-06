c89a883edbf92a167220b481e81e5183c3608bfe
may <may@470b2c59-2f82-4072-9d40-d544e64e7199>
2016-05-06 17:11:24 +0000

    Refs #1.58 - some todo models and views

---
+++ roadtrip/modules/todo/todo.js
@@ -0,0 +1,69 @@
+DEF.modules.todo = {
+	views: {}
+};
+DEF.modules.todo.Initialize = function () {
+	APP.Icon_Lookup.todo = "list";
+	if (!APP.models.todo)
+		APP.models.todo = new DEF.modules.todo.Collection();
+};
+DEF.modules.todo.Router = Roadtrip.Router.extend({
+	initialize: function () {},
+	collections: [
+		"todo", "users"
+	],
+	module: "todo",
+	routes: {
+		"todo": "ShowRoot",
+		"todo/:mode/:date": "ShowWeek"
+	},
+	ShowWeek: function (mode, date) {
+		var module = this.module;
+		APP.Page = new DEF.modules[module].MainView({
+			collection: APP.models[module],
+			date: date,
+			mode: mode
+		});
+		APP.root.showChildView("main", APP.Page);
+	}
+});
+
+
+DEF.modules.todo.Model = Roadtrip.Model.extend({
+	nameAttribute: 'task', // the human-readable field in the record
+	module: "todo",
+	defaults: {
+		_: {},
+		module: "order",
+		module_id: "",
+		task: "",
+		user: ""
+	},
+	search_string: function () {
+		return false;
+	}
+});
+
+DEF.modules.todo.Collection = Roadtrip.Collection.extend({
+	model: DEF.modules.todo.Model,
+	url: 'roadtrip.telegauge.com/roadtrip/todo',
+});
+
+/**
+ * A single line of todo on the main project view
+ */
+DEF.modules.todo.RecordLine = Roadtrip.RecordLine.extend({
+	module: "todo",
+	tagName: "tr",
+	className: 'click',
+	template: require("./templates/todoline.html"),
+});
+
+/**
+ * The MainView. A bunch of project boxes.
+ */
+DEF.modules.todo.MainView = Roadtrip.RecordList.extend({
+	id: 'todo',
+	template: require("./templates/todolist.html"),
+	childView: DEF.modules.todo.RecordLine,
+	childViewContainer: "#record_list",
+});
