a172dad5478f6095f989759c3897593035a72b3b
wck <wck@470b2c59-2f82-4072-9d40-d544e64e7199>
2016-05-12 18:49:11 +0000

    Restore the post-commit hook event log.  refs #1.15.4

--- roadtrip/modules/revisions/server.js
+++ roadtrip/modules/revisions/server.js
@@ -5,30 +5,28 @@ var hooks = {
 			var users = {
 				'wck': '56f3f7cb4b88de4618e306c0',
 				'may': '56fd29934b88de4618e306c5'
-			}
+			};
 
 			data.author = users[data.author];
 			success(data);
-		})
+		});
 	},
-	after2Save: function (data) {
+	afterSave: function (data) {
 		var self = this;
 		return new Promise(function (success, failure) {
-			console.log('I am the after save hook')
-			self.db.createRecord('events', {
+			self.db.createRecord({
 				"module": "revisions",
 				"module_id": data._id,
 				"event": "Revision committed",
 				"datetime": Date.now(),
 				"user_id": data.author
-			}).then(function (data) {
-				success(data)
+			}, 'events').then(function (data) {
+				success(data);
 			}, function (err) {
 				failure(err);
-			})
-
-		})
+			});
+		});
 	}
-}
+};
 
 module.exports = hooks;
