f806c2f21a935be64fc16bb235394ec1d71c7161
Warren Krewenki <krewenki@gmail.com>
2016-05-24 09:48:07 +0000

    Remove the cleanup method as mongojs uses auto_reconnect

--- server.js
+++ server.js
@@ -72,25 +72,3 @@ var hw = new Highway(config);
 hw.LoadRoutes(require('./modules/revisions/routes.js'));
 hw.LoadRoutes(require('./modules/repositories/routes.js'));
 //hw.LoadRoutes(require('./modules/tasks/routes.js'));
-
-function exitHandler(options, err) {
-	hw.CleanUp();
-	if (options.cleanup) console.log('clean');
-	if (err) console.log(err.stack);
-	if (options.exit) process.exit();
-}
-
-//do something when app is closing
-process.on('exit', exitHandler.bind(null, {
-	cleanup: true
-}));
-
-//catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {
-	exit: true
-}));
-
-//catches uncaught exceptions
-process.on('uncaughtException', exitHandler.bind(null, {
-	exit: true
-}));
